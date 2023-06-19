f0421e5e44914def98b0e5c7536678ca3c0f51cb
Lucas Oliveira <lucas.edu.oli@hotmail.com>
2023-06-19 16:14:25 -0300

    removed api key

--- src/frontend/src/controllers/API/index.ts
+++ src/frontend/src/controllers/API/index.ts
@@ -19,11 +19,6 @@ export async function getAll(): Promise<AxiosResponse<APIObjectType>> {
 }
 
 const GITHUB_API_URL = "https://api.github.com";
-const GITHUB_PERSONAL_ACCESS_TOKEN = "REDACTED_GITHUB_TOKEN";
-
-axios.defaults.headers.common[
-  "Authorization"
-] = `token ${GITHUB_PERSONAL_ACCESS_TOKEN}`;
 
 export async function getRepoStars(owner, repo) {
   try {
